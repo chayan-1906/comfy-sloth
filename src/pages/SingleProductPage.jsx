6f34bb048c83573f80877957fdd4f1a58d61beb7
Padmanabha Das <padmanabhadas9647@gmail.com>
2023-07-24 21:03:48 +0530

    single product loading error

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,21 +1,49 @@
 import React, {useEffect} from 'react'
-import {useParams} from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
 import {useProductsContext} from '../context/products_context.jsx'
-import { single_product_url as url } from '../utils/constants.jsx'
-import { formatPrice } from '../utils/helpers.jsx'
+import {single_product_url as url} from '../utils/constants.jsx'
+import {formatPrice} from '../utils/helpers.jsx'
 import {
-  Loading,
-  Error,
-  ProductImages,
-  AddToCart,
-  Stars,
-  PageHero,
+    Loading,
+    Error,
+    ProductImages,
+    AddToCart,
+    Stars,
+    PageHero,
 } from '../components/index.jsx'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
+    const {id} = useParams()
+    const navigate = useNavigate()
+    const {
+        single_product_loading: loading,
+        single_product_error: error,
+        single_product: product,
+        fetchSingleProduct,
+    } = useProductsContext()
+
+    useEffect(() => {
+        if (error) {
+            setTimeout(() => {
+                navigate(-1)
+            }, 3000)
+        }
+    }, [error])
+
+    useEffect(() => {
+        fetchSingleProduct(`${url}${id}`)
+    }, [id])
+    if (loading) {
+        return <Loading/>
+    }
+    if (error) {
+        return <Error/>
+    }
+    // Video #312 1:48
+
+    return <h4>single product page</h4>
 }
 
 const Wrapper = styled.main`
@@ -24,18 +52,22 @@ const Wrapper = styled.main`
     gap: 4rem;
     margin-top: 2rem;
   }
+
   .price {
     color: var(--clr-primary-5);
   }
+
   .desc {
     line-height: 2;
     max-width: 45em;
   }
+
   .info {
     text-transform: capitalize;
     width: 300px;
     display: grid;
     grid-template-columns: 125px 1fr;
+
     span {
       font-weight: 700;
     }
@@ -46,6 +78,7 @@ const Wrapper = styled.main`
       grid-template-columns: 1fr 1fr;
       align-items: center;
     }
+
     .price {
       font-size: 1.25rem;
     }
