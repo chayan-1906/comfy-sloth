9d46e140d0db03495ecea277da0dfacc55aa6897
Padmanabha Das <padmanabhadas9647@gmail.com>
2023-07-25 20:11:56 +0530

    single product stars arr appr

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -41,9 +41,38 @@ const SingleProductPage = () => {
     if (error) {
         return <Error/>
     }
-    // Video #312 1:48
 
-    return <h4>single product page</h4>
+    const {name, price, description, stock, stars, reviews, id: sku, company, images} = product
+
+    return (
+        <Wrapper>
+            <PageHero title={name} product/>
+            <div className='section section-center page'>
+                <Link to='/products' className='btn'>back to products</Link>
+                <div className='product-center'><ProductImages images={images}/></div>
+                <section className='content'>
+                    <h2>{name}</h2>
+                    <Stars stars={stars} reviews={reviews}/>
+                    <h5 className='price'>{formatPrice(price)}</h5>
+                    <p className='desc'>{description}</p>
+                    <p className='info'>
+                        <p>Available : </p>
+                        {stock > 0 ? 'in stock' : 'out of stock'}
+                    </p>
+                    <p className='info'>
+                        <p>SKU : </p>
+                        {sku}
+                    </p>
+                    <p className='info'>
+                        <p>Brand : </p>
+                        {company}
+                    </p>
+                    <hr/>
+                    {stock > 0 && <AddToCart/>}
+                </section>
+            </div>
+        </Wrapper>
+    )
 }
 
 const Wrapper = styled.main`
